cc4ed5fcd9c00623dce3982ce11ab82d2b9b06c6
slj <lijiao.song@aquiferre.com>
2024-01-11 18:02:00 +0800

    更新配置项

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,14 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
-   devServer: {
-    allowedHosts: ['all'],
+  devServer: {
+    allowedHosts: ["all"],
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
       "Cross-Origin-Opener-Policy": "same-origin",
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers":
+        "Origin, X-Requested-With, Content-Type, Accept",
     },
   },
 });

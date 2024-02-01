f6b96a94b19a356637a29b71b9a0207ed79b300d
slj <lijiao.song@aquiferre.com>
2024-02-01 11:21:46 +0800

    [feat]:添加提示信息

--- src/views/basicTools/BaseTool.vue
+++ src/views/basicTools/BaseTool.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="base">
     <h2>基础工具显示</h2>
+    <el-divider content-position="center"
+      >影像加载需要时间，请勿频繁切换页面导致请求回数据后找不到DOM，如果产生报错信息，刷新当前页面即可</el-divider
+    >
     <router-view />
   </div>
 </template>

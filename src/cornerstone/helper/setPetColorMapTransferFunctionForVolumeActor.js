cb64789ecc97d6c5aaaa96ffa0e0c3a698530909
slj <lijiao.song@aquiferre.com>
2024-07-19 14:04:01 +0800

    [feat]:新增PET模式案例及相关工具演示

--- src/cornerstone/helper/setPetColorMapTransferFunctionForVolumeActor.js
+++ src/cornerstone/helper/setPetColorMapTransferFunctionForVolumeActor.js
@@ -1,6 +1,5 @@
 import vtkColorMaps from "@kitware/vtk.js/Rendering/Core/ColorTransferFunction/ColorMaps";
 import vtkColorTransferFunction from "@kitware/vtk.js/Rendering/Core/ColorTransferFunction";
-import vtkPiecewiseFunction from "@kitware/vtk.js/Common/DataModel/PiecewiseFunction";
 
 export default function setPetColorMapTransferFunctionForVolumeActor(
   volumeInfo
@@ -17,10 +16,10 @@ export default function setPetColorMapTransferFunctionForVolumeActor(
   volumeActor.getProperty().setRGBTransferFunction(0, cfun);
 
   // Create scalar opacity function
-  const ofun = vtkPiecewiseFunction.newInstance();
-  ofun.addPoint(0, 0.0);
-  ofun.addPoint(0.1, 0.9);
-  ofun.addPoint(5, 1.0);
-
-  volumeActor.getProperty().setScalarOpacity(0, ofun);
+  // const ofun = vtkPiecewiseFunction.newInstance();
+  // ofun.addPoint(0, 0.0);
+  // ofun.addPoint(0.1, 0.9);
+  // ofun.addPoint(5, 1.0);
+  //
+  // volumeActor.getProperty().setScalarOpacity(0, ofun);
 }
